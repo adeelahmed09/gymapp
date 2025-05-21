17644fb2cf43552db733dfe635cef8f49a364a33
adeelahmed9 <adeel0ahmed10@gmail.com>
2025-05-22 01:20:02 +0500

    Update Contact Us link in Nav component to point to the correct section

--- src/app/components/Nav.js
+++ src/app/components/Nav.js
@@ -7,7 +7,7 @@ function Nav() {
     <div className='bg-[#0D0D0D] border-[1px] border-solid border-[#62BF04]  text-[#62BF04] flex items-center justify-around font-semibold sm:w-[40vw] w-[90vw] sm:h-[10vh] h-[7vh] rounded-3xl'>
       <Link className=' hover:scale-[1.1] hover:text-white' href={"#hero"}>Home</Link>
       <Link className=' hover:scale-[1.1] hover:text-white' href={"#about-us"}>About Us</Link>
-      <Link className=' hover:scale-[1.1] hover:text-white' href={"#"}>Contact Us</Link>
+      <Link className=' hover:scale-[1.1] hover:text-white' href={"#contact-us"}>Contact Us</Link>
     </div>
   )
 }
